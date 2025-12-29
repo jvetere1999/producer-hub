5d143317fffac78efcc06c59f559f75cdc82c24e
Jacob Vetere <jvetere@aohwv.com>
2025-12-28 20:13:10 -0500

    chore: update revision hash for `200.html` in service worker
    - Updated the cached revision hash for `200.html` to ensure proper cache invalidation and retrieval of the latest content.
    - Retained cleanup of outdated caches and registration of navigation routes for consistent service worker behavior.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "200.html",
-    "revision": "0.qaq92k9aj48"
+    "revision": "0.mfivng6od78"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("200.html"), {
