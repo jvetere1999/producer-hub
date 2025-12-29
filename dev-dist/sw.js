cd07faa2d24cfa20a2d90d299d8fbbc8bed95aeb
Jacob Vetere <jvetere@aohwv.com>
2025-12-28 20:48:50 -0500

    chore: update cached revision for `200.html` and add GPLv3 license
    - Updated the revision hash for `200.html` in the service worker to ensure proper cache invalidation and retrieval of the latest content.
      - Retained cleanup of outdated caches and registration of navigation routes for consistent service worker behavior.
    - Added the GNU General Public License v3 (GPLv3) to the project for clear licensing and compliance.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "200.html",
-    "revision": "0.mfivng6od78"
+    "revision": "0.tobeqqdc47g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("200.html"), {
